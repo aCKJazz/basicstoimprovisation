0de97921c6d5cb97265afe05950a9bc2944e498f
yoggsoft <yoggsoft@gmail.com>
2018-05-12 15:28:50 +0200

    Move index, Add Readme

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,8 @@ module.exports = {
     extensions: ['*', '.js', '.jsx']
   },
   output: {
-    path: path.join(__dirname, '/dist'),
+    // path: path.join(__dirname, '/dist'),
+    path: __dirname,
     publicPath: '/',
     filename: 'bundle.js'
   },
